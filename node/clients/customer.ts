5b51bb407c835f65013741d06c44407dddf2d2bd
Sivasankaris1 <sivasankari.s@trikatechnologies.com>
2023-09-13 16:59:16 +0530

    duplicated file removed

--- node/clients/customer.ts
+++ node/clients/customer.ts
@@ -10,14 +10,15 @@ export default class Customer extends ExternalClient {
     }
 
     public async createCustomerData(payload: any){
-        console.log("apyload" + JSON.stringify(payload))
         const savedData = await this.http.post<Promise<any>>(
-            `/api/dataentities/CL/documents`,payload
+            `/api/dataentities/CL/documents`,payload,
+            await this.getHeaders()
         )
         console.log("saved" + savedData)
         if (savedData) {
             return {
-              success: true
+              success: true,
+              data: savedData
             }
         } else {
             return savedData
@@ -26,14 +27,17 @@ export default class Customer extends ExternalClient {
 
     public async getCustomerData() {
         const customerData = await this.http.get<Promise<any>>(
-            `/api/dataentities/CL/Search?_fields=name,email`,
+            `/api/dataentities/CL/Search?_fields=DocumentId,name,email`,
             await this.getHeaders()
         )
         return customerData
     }
 
     public async updateCustomerData(payload: any, id: string) {
-        const updateCustomerData = await this.http.patch<Promise<any>>(
+        // console.log("in update")
+        // const headersData = await this.getHeaders()
+        // console.log("headers in create" + JSON.stringify(headersData))
+        const updateCustomerData = await this.http.put<Promise<any>>(
           `/api/dataentities/CL/documents/${id}`, payload,
           await this.getHeaders()
         )
@@ -57,7 +61,6 @@ export default class Customer extends ExternalClient {
     private async getHeaders() {
         const App = new Apps(this.context)
         this.setting = await App.getAppSettings(process.env.VTEX_APP_ID ?? '')
-        console.log(this.setting)
         return {
             headers : {
                 'Content-type': 'application/json',
